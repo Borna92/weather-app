044b9224420fad04d0f0f8d1eda091837f45fc70
Borna92 <canicborna@gmail.com>
2023-06-28 15:06:47 -0400

    adding materialUI

--- src/Today.jsx
+++ src/Today.jsx
@@ -1,5 +1,10 @@
 import { getIcon } from './Day';
 import { BsWind } from 'react-icons/bs';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 function Today({
   timezone,
@@ -12,6 +17,12 @@ function Today({
   coords,
 }) {
   const uvIndex = daily.uv_index_max[0];
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
 
   return (
     <div className="today-display">
@@ -46,17 +57,25 @@ function Today({
           </i>
           {current_weather.windspeed} km/h
         </span>
-        <select
-          className="form-select"
-          value={days}
-          onChange={(e) => setDays(e.target.value)}
-        >
-          <option value="1">1 day</option>
-          <option value="3">3 days</option>
-          <option value="7">7 days</option>
-          <option value="14">14 days</option>
-          <option value="16">16 days</option>
-        </select>
+        <ThemeProvider theme={darkTheme}>
+          <FormControl fullWidth>
+            <InputLabel id="days">Days</InputLabel>
+            <Select
+              // sx={{ color: '#fff' }}
+              labelId="days"
+              id="values"
+              value={days}
+              label="Days"
+              onChange={(e) => setDays(e.target.value)}
+            >
+              <MenuItem value={1}>1 day</MenuItem>
+              <MenuItem value={3}>3 days</MenuItem>
+              <MenuItem value={7}>7 days</MenuItem>
+              <MenuItem value={14}>14 days</MenuItem>
+              <MenuItem value={16}>16 days</MenuItem>
+            </Select>
+          </FormControl>
+        </ThemeProvider>
       </div>
     </div>
   );
